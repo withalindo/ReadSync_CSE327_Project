8aa5ca4fe842850d0c6ebc0132133b56d76a3ea3
withalindo <withalindo@gmail.com>
2025-04-18 23:21:32 +0600

    Documentation Updated.

--- Documentation/html/search/all_0.js
+++ Documentation/html/search/all_0.js
@@ -7,10 +7,9 @@ var searchData=
   ['_24check_5fauthor_5fquery_4',['$check_author_query',['../addbooks_8php.html#a2b970eb0ea99d5a65a085e96ee542ad8',1,'addbooks.php']]],
   ['_24check_5fcategory_5fquery_5',['$check_category_query',['../addbooks_8php.html#a41fadd88e9dd0816e1d7d020c4a6830f',1,'addbooks.php']]],
   ['_24checkbookquery_6',['$checkBookQuery',['../update_book_name_8php.html#aec5d0c9b0bc65902d56f4aa2ff93efa3',1,'$checkBookQuery:&#160;updateBookName.php'],['../update_book_price_8php.html#aec5d0c9b0bc65902d56f4aa2ff93efa3',1,'$checkBookQuery:&#160;updateBookPrice.php']]],
-  ['_24conn_7',['$conn',['../update_book_copy_8php.html#aa8a5a87b9c1a6a0819b88447cbe41877',1,'updateBookCopy.php']]],
-  ['_24dbname_8',['$dbName',['../connect_d_b_8php.html#a68f39949e76b64662a06cb56579d91c3',1,'connectDB.php']]],
-  ['_24dbpass_9',['$dbPass',['../connect_d_b_8php.html#a30d808caf55f524798c6a5aaafd633ad',1,'connectDB.php']]],
-  ['_24dbserver_10',['$dbServer',['../connect_d_b_8php.html#aa26867126e25e19a72436bf08d8af582',1,'connectDB.php']]],
-  ['_24dbuser_11',['$dbUser',['../connect_d_b_8php.html#a4a92606de85aafdc0dcae4976b7ca669',1,'connectDB.php']]],
-  ['_24sql_12',['$sql',['../remove_user_8php.html#a047170d6020a882807665812a27e2525',1,'removeUser.php']]]
+  ['_24dbname_7',['$dbName',['../connect_d_b_8php.html#a68f39949e76b64662a06cb56579d91c3',1,'connectDB.php']]],
+  ['_24dbpass_8',['$dbPass',['../connect_d_b_8php.html#a30d808caf55f524798c6a5aaafd633ad',1,'connectDB.php']]],
+  ['_24dbserver_9',['$dbServer',['../connect_d_b_8php.html#aa26867126e25e19a72436bf08d8af582',1,'connectDB.php']]],
+  ['_24dbuser_10',['$dbUser',['../connect_d_b_8php.html#a4a92606de85aafdc0dcae4976b7ca669',1,'connectDB.php']]],
+  ['_24sql_11',['$sql',['../remove_user_8php.html#a047170d6020a882807665812a27e2525',1,'removeUser.php']]]
 ];
--- Documentation/html/update_book_copy_8php.js
+++ Documentation/html/update_book_copy_8php.js
@@ -1,5 +1,4 @@
 var update_book_copy_8php =
 [
-    [ "updateBookCopy", "classupdate_book_copy.html", "classupdate_book_copy" ],
-    [ "$conn", "update_book_copy_8php.html#aa8a5a87b9c1a6a0819b88447cbe41877", null ]
+    [ "updateBookCopy", "classupdate_book_copy.html", "classupdate_book_copy" ]
 ];
